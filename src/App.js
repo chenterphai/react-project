07b28c17b5c1d950177112ce94f4228ba33b97e6
Chenter Phai <chenterphai61@gmail.com>
2024-03-04 20:21:40 -0800

    Updated!

--- src/App.js
+++ src/App.js
@@ -2,29 +2,62 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // Constant variables
+  const title = "Welcome to new blog!"
+  const likes = 50
+
+  
+  // Object
+  const person = {
+    name: 'Chenter',
+    age: 19
+  }
+
+
+  // Array
+  const Employees = [
+    {
+      name: 'Vireak',
+      age: 20,
+      pos: 'Web Designer'
+    },
+    {
+      name: 'Boeun Boek',
+      age: 22,
+      pos: 'Web Developer'
+    },
+    {
+      name: 'Ny Vannak',
+      age: 21,
+      pos: 'Graphic Designer'
+    },
+  ]
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
 
-      <main>
+      <main className='container mx-auto border py-4 mt-2'>
         <h1 className='text-red-600'>Hello World!</h1>
+        <h1>{title}</h1>
+        <p>Likes {likes} times.</p>
+        <h1>Hi! My name is {person.name}</h1>
+        <p>I am {person.age}</p>
+
+        {Employees.map((items) => (
+          <div>
+            <h1>{`Name: ${items.name}.`}</h1>
+            <p>{`Age: ${items.age}`}</p>
+            <p>{`Position: ${items.pos}`}</p>
+          </div>
+        ))}
       </main>
 
     </div>
   );
+
+  
 }
 
 export default App;
